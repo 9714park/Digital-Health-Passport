0677d86313bc4a55d2be58dcdfd3e9c295e59212
상민 박 <smpark95@mindslab.ai>
2021-03-01 17:02:02 -0500

    Initial setup.

---
+++ dhp/src/main/java/com/hackathon/dhp/DigitalHealthPassportApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.dhp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalHealthPassportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalHealthPassportApplication.class, args);
+    }
+
+}
