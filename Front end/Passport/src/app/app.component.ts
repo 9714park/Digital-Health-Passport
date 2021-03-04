f3a02b832096db4e7f678646fa1044999b6b034b
Leyang Yu <lyu80@myseneca.ca>
2021-03-03 23:49:57 -0500

    Removed header and tabs for login

--- Front end/Passport/src/app/app.component.ts
+++ Front end/Passport/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  url: string;
+  authenticated: boolean;
+  urls = ["/home", "/passport", "/vaccine-history", "/vaccine-register", "/covid-test", "/covid-register"];
+
+  constructor(private location: Location) {
+    location.onUrlChange(e => {
+      this.url = e;
+      this.authenticated = this.urls.includes(this.url) ? true : false;
+    });
+  }
 }
